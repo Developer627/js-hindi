789098e6ab939041fd8493e0dd5b966e4ecbc617
Bharat Singh <56514765+Bharatastic@users.noreply.github.com>
2023-10-20 18:02:51 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1); // => true
+// console.log(2 >= 1); // => true
+// console.log(2 < 1); // => false
+// console.log(2 == 1); // => false
+// console.log(2 != 1); // => true
+
+
+// console.log("2" > 1); // => true
+// console.log("02" > 1); // => true
+
+// console.log(null > 0); // => false - Avoid This
+// console.log(null == 0); // => false - Avoid This
+// console.log(null >= 0); // => true - Avoid This
+
+// console.log(undefined == 0); // => false - Avoid This
+// console.log(undefined === 0); // => false - Avoid This
+// console.log(undefined > 0); // => false - Avoid This
+// console.log(undefined >= 0); // => false - Avoid This
+// console.log(undefined < 0); // => false - Avoid This
+// console.log(undefined <= 0); // => false - Avoid This
+
+// ===
+
+console.log("2" === 2); // => false
