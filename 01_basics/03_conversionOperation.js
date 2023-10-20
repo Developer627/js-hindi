8b19c39e949bc34e07a3cd5f14fa558be9baf370
Developer627 <126055157+Developer627@users.noreply.github.com>
2023-10-20 13:05:42 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN => Not a Number
+// true => 1; false => 0
+
+let isLoggedin = "doge";
+
+let booleanisLoggedin = Boolean(isLoggedin);
+// console.log(booleanisLoggedin);
+
+// 1 => true; 0 => false
+// "" => false
+// "doge" => true
+
+let someNumner = 33;
+
+let stringNumber = String(someNumner);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
