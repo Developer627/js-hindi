22c7f4363ae938349672cf876bccb0c5dbe12469
Bharat Singh <56514765+Bharatastic@users.noreply.github.com>
2023-10-22 09:47:12 +0000

    Data Types Summary

---
+++ datatypes-summary.js
@@ -0,0 +1,46 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id);
+// console.log(anotherId);
+// console.log(id === anotherId);
+
+
+// const BigNumber = 2389472309472089372935723423423024234n;
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktimaan", "Naagraj", "Doga"];
+
+let myObj = {
+    name: "Cheems",
+    age: 69,
+};
+
+const myFunction = function() {
+    console.log("Hemllo Worlmd!");
+}
+
+console.log("score: " + typeof(score));
+console.log("scoreValue: " + typeof(scoreValue));
+console.log("isLoggedIn: " + typeof(isLoggedIn));
+console.log("outsideTemp: " + typeof(outsideTemp));
+console.log("userEmail: " + typeof(userEmail));
+console.log("id: " + typeof(id));
+console.log("anotherId: " + typeof(anotherId));
+console.log("heros: " + typeof(heros));
+console.log("myObj: " + typeof(myObj));
+console.log("myFunction: " + typeof(myFunction));
