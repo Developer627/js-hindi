055f2ddc4ef65be30debd1e5bf6f3856baf9a24d
Developer627 <126055157+Developer627@users.noreply.github.com>
2023-11-07 12:25:40 +0000

    JSON Talk & Object De-Structuring

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,18 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseinstructor: "Vidhayak"
+}
+
+// course.courseinstructor;
+
+const {courseinstructor: instructor} = course
+
+// console.log(courseinstructor);
+console.log(instructor);
+
+// {
+//     "name": "Cheems",
+//     "coursename": "js in hindi",
+//     "price": "free",
+// }
